5b5bd9f173af4c13ad669a9231208db5c0ad468d
dfederico <dfederico7@gmail.com>
2015-03-04 20:35:48 -0600

    Adding front end functionality

--- express_test/server.js
+++ express_test/server.js
@@ -2,9 +2,18 @@ var express = require('express')
 var request = require('request')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// });
+app.get('*', function (req, res) {
+  res.sendFile('./public/index.html', {"root": __dirname}); // load our public/index.html file
+  // res.send('test');
 });
+
+// app.get('/', function (req, res) {
+//   res.send('wut wut');
+// })/
+
 
 app.get('/spring', function (req, res) {
 	request('http://rest-service.guides.spring.io/greeting', function (error, response, body) {
@@ -14,6 +23,16 @@ app.get('/spring', function (req, res) {
   		}
 	})
 });
+
+app.get('/summoner', function (req, res) {
+  request('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/biffwidefelo?api_key=33ddb3c0-b554-4c7e-a274-fbf483eb2afb', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body)
+        res.send(body);
+      }
+  })
+});
+
 
 // http://rest-service.guides.spring.io/greeting
 
