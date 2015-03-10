a0b4dd0bef9139ec67a20527e6fd02d09d75b31a
dfederico <dfederico7@gmail.com>
2015-03-09 20:52:12 -0500

    adding bummoner functionality

--- express_test/server.js
+++ express_test/server.js
@@ -21,10 +21,23 @@ app.get('/spring', function (req, res) {
 	})
 });
 
-app.get('/summoner', function (req, res) {
-  request('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/biffwidefelo?api_key=33ddb3c0-b554-4c7e-a274-fbf483eb2afb', function (error, response, body) {
+app.get('/bummoner/:id', function (req, res) {
+  console.log(req.params.id);
+  // var summ = 'biffwidefelo';
+  var summ = req.params.id;
+  request('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + summ + '?api_key=33ddb3c0-b554-4c7e-a274-fbf483eb2afb', function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        console.log(body)
+        console.log(body);
+        res.send(body);
+      }
+  })
+});
+
+app.get('/summoner', function (req, res) {
+  var summ = 'biffwidefelo';
+  request('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + 'biffwidefelo' + '?api_key=33ddb3c0-b554-4c7e-a274-fbf483eb2afb', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
         res.send(body);
       }
   })
