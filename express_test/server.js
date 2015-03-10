03b82e4bda8b8e3a2b54f4b2068b101cbc559b45
dfederico <dfederico7@gmail.com>
2015-03-09 21:32:08 -0500

    Adding functionality to select summoner ID

--- express_test/server.js
+++ express_test/server.js
@@ -22,13 +22,30 @@ app.get('/spring', function (req, res) {
 });
 
 app.get('/bummoner/:id', function (req, res) {
-  console.log(req.params.id);
-  // var summ = 'biffwidefelo';
   var summ = req.params.id;
-  request('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + summ + '?api_key=33ddb3c0-b554-4c7e-a274-fbf483eb2afb', function (error, response, body) {
+  var req1string = 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + summ + '?api_key=33ddb3c0-b554-4c7e-a274-fbf483eb2afb';
+  // request('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + summ + '?api_key=33ddb3c0-b554-4c7e-a274-fbf483eb2afb', function (error, response, body) {
+    request(req1string, function (error, response, body) {  
       if (!error && response.statusCode == 200) {
-        console.log(body);
-        res.send(body);
+        var hold = JSON.parse(body);
+        var data;
+        for (var property in hold) {
+          if (hold.hasOwnProperty(property)) {
+            data = hold[property];
+            break;
+          }
+        }
+        console.log(data['id']);
+        var summId = data['id'];
+        // var req2string = 'https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/' + 36882426 + '?api_key=33ddb3c0-b554-4c7e-a274-fbf483eb2afb';
+        var req2string = 'https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/' + summId + '?api_key=33ddb3c0-b554-4c7e-a274-fbf483eb2afb';
+
+        request(req2string, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            res.send(body);
+          }
+        })
+        // res.send(body);
       }
   })
 });
@@ -37,7 +54,7 @@ app.get('/summoner', function (req, res) {
   var summ = 'biffwidefelo';
   request('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + 'biffwidefelo' + '?api_key=33ddb3c0-b554-4c7e-a274-fbf483eb2afb', function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        console.log(body);
+        // console.log(body);
         res.send(body);
       }
   })
