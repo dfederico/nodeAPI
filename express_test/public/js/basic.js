5af78d3bd4691f0a30b26e8fed9061ef6f41c184
dfederico <dfederico7@gmail.com>
2015-05-11 22:11:13 -0500

    Loading underscore and creating functionality to grab static data and inject into display

--- express_test/public/js/basic.js
+++ express_test/public/js/basic.js
@@ -8,6 +8,8 @@ angular.module('basic', [])
 				success(function (data, status, headers, config) {
 					champJSON = data.data;
 					console.log('the champ json', champJSON);
+					// var test = _.where(champJSON, 'id');
+					// console.log(test);
 				}).
 				error(function (data, status, headers, config) {
 
@@ -23,10 +25,11 @@ angular.module('basic', [])
 			    var holder = data.matches;
 			    console.log('dan fed', holder.length);
 			    for(var i = 0; i < holder.length; i++) {
-			    	console.log(holder[i].participants[0].championId);
+			    	var tempObject = _.where(champJSON, {id:holder[i].participants[0].championId});
+			    	holder[i].participants[0].championId = tempObject[0].name;
 			    }
 
-			    $scope.games = data.matches;
+			    $scope.games = holder;;
 			  }).
 			  error(function (data, status, headers, config) {
 			  	console.log('failed');
